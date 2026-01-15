1ef18bd32e1959afd8e10666cbbf7978a2bf9eab
janghan ha(Mac) <egnever4434@ajou.ac.kr>
2026-01-15 14:56:21 +0900

    Fix: apply .gitignore and remove tracked cache files

--- C_chat_lib/build/CMakeFiles/copy_library.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_library"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
